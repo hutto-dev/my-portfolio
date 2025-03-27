10ca349fe49f2ca378ec55c88b93d487215ffa59
hutto-dev <183306074+huttodev@users.noreply.github.com>
2025-03-26 21:03:05 -0400

    First edit on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -15,36 +15,10 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            <code>May 12, 2024</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li> :)</li>
         </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <footer className={styles.footer}>
         <a
